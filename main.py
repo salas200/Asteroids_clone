ff2585e50bd859e3d35844cf7f9024c8f8a3c7e9
salas200 <salasdylan50@gmail.com>
2025-06-16 19:28:28 -0700

    finalized player movement

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    # creates player at center of screen
+
+    # creates player into groups and places at center of screen
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
 
     # creates a forever loop that will allow to exit the game 
@@ -18,10 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)    
+        updatable.update(dt)    
 
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         
 
