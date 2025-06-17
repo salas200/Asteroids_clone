5d71ddbe86e15f5deefba360537179f99f6d9bc5
salas200 <salasdylan50@gmail.com>
2025-06-16 18:39:23 -0700

    created player

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from constants import *
-
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+
+    # creates a forever loop that will allow to exit the game 
+    # also update the screen to always show black foreground
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+
+        # Limits the framerate
+        dt = clock.tick(60) / 1000
+
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
